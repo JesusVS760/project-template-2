0b104809a17de2c054ba09e75248c83eb1539746
JesusVA760 <tonio4340406@gmail.com>
2024-06-15 22:04:51 -0700

    gallary

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import arrow from "./assets/images/arrow.png";
+import teen from "./assets/images/teen.jpg";
+import teen2 from "./assets/images/teen2.jpg";
+import teen3 from "./assets/images/teen3.jpg";
 import "./App.css";
 const App = () => {
   return (
@@ -25,6 +28,27 @@ const App = () => {
               Donate Now <img src={arrow} alt="arrow" />
             </button>
           </div>
+        </div>
+      </div>
+      <div className="landing-gallary">
+        <div className="gallary-img">
+          <div className="img-phrase">
+            {" "}
+            <p>One</p>
+          </div>
+          <img src={teen} alt="" />
+        </div>
+        <div className="gallary-img">
+          <div className="img-phrase">
+            <p> Student at a</p>
+          </div>
+          <img src={teen2} alt="" />
+        </div>
+        <div className="gallary-img">
+          <div className="img-phrase">
+            <p>Time.</p>
+          </div>
+          <img src={teen3} alt="" />
         </div>
       </div>
     </div>
