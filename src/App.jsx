7c2c7273d9aeacf162db8b2cc5135a3db029cd68
JesusVA760 <tonio4340406@gmail.com>
2024-06-15 23:12:13 -0700

    Causes

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import arrow from "./assets/images/arrow.png";
 import teen from "./assets/images/teen.jpg";
 import teen2 from "./assets/images/teen2.jpg";
 import teen3 from "./assets/images/teen3.jpg";
+import idea from "./assets/images/idea.png";
+import mental from "./assets/images/mental.png";
+import rate from "./assets/images/rate.png";
 import "./App.css";
 const App = () => {
   return (
@@ -49,6 +52,47 @@ const App = () => {
             <p>Time.</p>
           </div>
           <img src={teen3} alt="" />
+        </div>
+      </div>
+      <div className="landing-causes">
+        <div className="causes-header">
+          <h1>Our Work to Mitigate.</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vel
+            libero sem. Donec id diam vitae eros ultricies porta.{" "}
+          </p>
+        </div>
+        <div className="causes-stats">
+          <div className="stats-content">
+            <img src={idea} alt="" />
+            <div className="stats-para">
+              <h3>Education and Literacy:</h3>
+              <p>
+                Preventing dropout rates through mentorship and academic support
+                programs.
+              </p>
+            </div>
+          </div>
+          <div className="stats-content">
+            <img src={mental} alt="" />
+            <div className="stats-para">
+              <h3>Mental Health and Counseling</h3>
+              <p>
+                Early intervention and awareness programs to prevent mental
+                health crises.
+              </p>
+            </div>
+          </div>
+          <div className="stats-content">
+            <img src={rate} alt="" />
+            <div className="stats-para">
+              <h3>Life Skills and Personal Development</h3>
+              <p>
+                Offering life skills education and leadership development to
+                prevent future challenges.
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
